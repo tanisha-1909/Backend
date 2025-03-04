05c5dad4704f0759a8038a08469c27824ce74748
tanisha-1909 <tanishamahavar32@gmail.com>
2025-03-04 20:41:47 +0530

    using figlet

---
+++ figlet/index.js
@@ -0,0 +1,11 @@
+const figlet= require("figlet");
+
+
+figlet("Tanisha Mahavar", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
