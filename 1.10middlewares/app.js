a6761b51705161ad71988d5d64e22053ab988364
tanisha-1909 <tanishamahavar32@gmail.com>
2025-04-04 16:55:54 +0530

    using next and logger-utility middleware

--- 1.10middlewares/app.js
+++ 1.10middlewares/app.js
@@ -10,15 +10,44 @@ const app=express();
 
 // req,res with middlwware
 // this middlware is getting used for all routes
-app.use((req,res)=>{
-    let {query}= req.query;
-    // http://localhost:3000/test?query=%tanisha%
-    // "tanisha
-    console.log(query);
+// app.use((req,res)=>{
+//     let {query}= req.query;
+//     // http://localhost:3000/test?query=%tanisha%
+//     // "tanisha
+//     console.log(query);
 
-    console.log("middlware working");
-    res.send("I am middleware");
+//     console.log("middlware working");
+//     res.send("I am middleware");
+// })
+
+// ----------------middlware stack execution  using next
+// next() in middleware passes control to the next middleware function in the request-response cycle.
+// app.use((req,res,next)=>{
+//     console.log("middleware 1")
+//     next();
+//     console.log("backtrack");
+//     // not a good practice though
+// });
+
+// app.use((req,res,next)=>{
+//     console.log("middleware 2")
+//     next();
+// })
+
+// o/p
+// middleware 1
+// middleware 2
+// backtrack
+
+// -------------------------
+
+
+// --------utility middleware(logger)
+app.use((req,res,next)=>{
+    console.log(req.method, req.hostname , req.path);
+    next();
 })
+// GET localhost / (o/p)
 
 app.listen("3000",()=>{
     console.log("server listening to port");
